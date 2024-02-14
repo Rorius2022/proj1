b49da99c4b2a9b293c512eccf3f208090a351a10
Rorius <naroviukr3@gmail.com>
2024-02-14 19:43:03 +0200

    testing loops

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("test!!!!!!!!!!!!!!!");
+let data1 = {
+    name: "John",
+    pass: "sfgsdg"
+}
+
+let data = [
+    {
+        name: "John",
+        pass: "sfgsdg"
+    }
+    ,
+    {
+        name: "Erik",
+        pass: "sdfasf"
+    }
+];
+
+for(let key in data)
+{
+    console.log(key);
+}
